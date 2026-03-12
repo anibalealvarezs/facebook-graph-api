1d1915c7989c910e7a8a07334c656fbe58708084
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-03-12 10:48:59 -0400

    fix: remove fields causing 500 error in BUSINESS_MANAGEMENT permission

--- src/Enums/PagePermission.php
+++ src/Enums/PagePermission.php
@@ -20,7 +20,7 @@ enum PagePermission: string
             self::PAGES_READ_ENGAGEMENT => 'about,description,fan_count,cover,location,phone,website,email,hours,is_permanently_closed,verification_status,business,engagement,followers_count,new_like_count,rating_count,overall_star_rating,affiliation,company_overview,contact_address,founded,general_info,mission,products',
             self::PAGES_READ_USER_CONTENT => '',
             self::PAGES_MANAGE_METADATA => 'access_token',
-            self::BUSINESS_MANAGEMENT => 'business,merchant_settings,attribution_details,created_by,created_time,updated_by,updated_time,instagram_business_account',
+            self::BUSINESS_MANAGEMENT => 'business,merchant_settings,instagram_business_account',
         };
     }
 
