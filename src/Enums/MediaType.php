b7a3ee519b85f3e09ee666d92a60a968995e141e
Aníbal Alvarez <anibalealvarezs@gmail.com>
2026-03-31 09:45:01 -0400

    Updated MediaType enum to include REEL and impressions/views/total_interactions for all IG media types.

--- src/Enums/MediaType.php
+++ src/Enums/MediaType.php
@@ -10,14 +10,16 @@ enum MediaType: string
     case IMAGE = 'IMAGE';
     case VIDEO  = 'VIDEO';
     case CAROUSEL_ALBUM = 'CAROUSEL_ALBUM';
+    case REEL = 'REEL';
 
     public function insightsFields(MetricSet $set = MetricSet::BASIC): string
     {
         $basic = match ($this) {
             self::IMAGE,
-            self::CAROUSEL_ALBUM => 'comments,likes,reach,saved,shares,total_interactions,views',
-            self::VIDEO => 'reach,saved',
-            default => 'comments,ig_reels_avg_watch_time,ig_reels_video_view_total_time,likes,reach,saved,shares,total_interactions,views',
+            self::VIDEO,
+            self::REEL,
+            self::CAROUSEL_ALBUM => 'comments,likes,reach,saved,shares,total_interactions,views,impressions',
+            default => 'comments,ig_reels_avg_watch_time,ig_reels_video_view_total_time,likes,reach,saved,shares,total_interactions,views,impressions',
         };
         return match ($set) {
             MetricSet::BASIC, MetricSet::KEY, MetricSet::FULL => $basic,
